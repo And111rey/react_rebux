c33ec1ae3dd348bd0b22a90fd89226b7f5e26c90
Andrey <samu111ray@gmail.com>
2019-11-02 22:52:37 +0200

    first

---
+++ my-appls/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtVjF1CLJgI8vl_KLj2w4OKLnVdb2U2DjTUS8El2giZcsMqtw5&s"/>
+        </header>
+    )
+}
+
+
+export { Header }
